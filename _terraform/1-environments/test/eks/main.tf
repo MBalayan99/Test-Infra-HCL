6d73da298357fc0900d0e62b1296c82971290d48
mher <mher@localhost.localdomain>
2025-12-19 19:32:03 +0400

    fix eks.yml $ push .tf

--- _terraform/1-environments/test/eks/main.tf
+++ _terraform/1-environments/test/eks/main.tf
@@ -6,8 +6,18 @@ module "this" {
 
   account_id = "718484331636"
   alarms = {"sns_topic":"Default"}
+  cluster_enabled_log_types = []
   cluster_name = "eks-dev"
+  enable_kube_state_metrics = true
+  karpenter = {"configs":{"replicas":1},"enabled":true}
+  keda = {"attach_policies":{"sqs":true},"enabled":false}
   map_users = [{"groups":["system:masters"],"rolearn":"arn:aws:iam::718484331636:role/login-aws-check-role","username":"AWSReservedSSO_AdministratorAccess_9f6025a800250f00"}]
+  node_groups = {"dev_nodes_ondemand":{"ami_type":"AL2023_x86_64_STANDARD","block_device_mappings":{"xvda":{"device_name":"/dev/xvda","ebs":{"volume_size":100,"volume_type":"gp3"}}},"capacity_type":"ON_DEMAND","desired_size":1,"instance_types":["t2.micro"],"max_capacity":1,"max_size":1,"min_size":1},"node_groups_default":{"ami_type":"AL2023_x86_64_STANDARD","capacity_type":"SPOT","disk_size":50,"instance_types":["t2.micro"]}}
+  prometheus_metrics = ["kube_node_status_condition"]
+  resource_configs = {"nodePools":{"general":{"weight":1}}}
+  resource_configs_defaults = {"limits":{"cpu":100}}
+  s3_csi = {"enabled":false}
+  vpc = {"link":{"id":"$(data.tfe_outputs.this["1-environments/test/vpc"].values.results.id)","private_subnet_ids":"$(data.tfe_outputs.this["1-environments/test/vpc"].values.results.private_subnets)"}}
   providers = {"aws":"aws"}
 }
 
