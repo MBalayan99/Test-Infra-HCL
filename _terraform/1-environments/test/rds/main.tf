a5363feb51086f920d6b3dec854998ec44872e0a
mher <mher@localhost.localdomain>
2025-12-23 16:46:56 +0400

    try fix

--- _terraform/1-environments/test/rds/main.tf
+++ _terraform/1-environments/test/rds/main.tf
@@ -14,7 +14,6 @@ module "this" {
   engine = "mysql"
   engine_version = "8.0"
   identifier = "rds-instance"
-  ingress_with_cidr_blocks = {"cidr_blocks":"10.16.0.0/16","from_port":3306,"protocol":"tcp","to_port":3306}
   instance_class = "db.t2.micro"
   multi_az = false
   port = 3306
@@ -23,7 +22,7 @@ module "this" {
   security_group_name = "rds-instance-sg"
   storage_encrypted = true
   storage_type = "gp2"
-  subnet_ids = "${linked_workspaces.vpc.outputs.private_subnets}"
+  subnet_ids = "${data.tfe_outputs.this["1-environments/test/vpc"].values.results.private_subnets}"
   tags = {"Environment":"test","Owner":"mher-test","Project":"Test-Infra-HCL"}
   providers = {"aws":"aws"}
 }
